e6f8691bbefa152cd1be6636cec94cbca997d86a
sugarless <quangnd@foobla.com>
2019-06-01 14:40:42 +0700

    .

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -16,7 +16,6 @@ exports.editProfile = (req, res) => {
 
 exports.addField = (req, res) => {
     const {id , field} = {...req.body, ...req.params}
-    console.log(field)
     teacherActions.addField({id, field})
         .then(data => res.send(data))
         .catch(err => res.send({ success: false, message: err.message || err }))
